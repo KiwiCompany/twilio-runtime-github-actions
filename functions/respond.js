679c4aab4adbdb210873f7889cff8eeb615b5505
Anthony Medina <anthonymedina958@gmail.com>
2024-06-10 12:12:19 -0400

    Feat: Calls transfers

--- functions/respond.js
+++ functions/respond.js
@@ -7,72 +7,28 @@ const { Readable } = require("stream");
 exports.handler = async function(context, event, callback) {
     const openai = new OpenAI({ api_key: context.OPENAI_API_KEY});
     const twiml = new Twilio.twiml.VoiceResponse();
-
-    let thread_id = event.thread_id
-
-    if(!thread_id){
-        let _thread = await openai.beta.threads.create();
-        thread_id = _thread.thread_id
-    }
-    
-    // const base64encodedData = Buffer.from(context.TWILIO_ACCOUNT_SID + ':' + context.TWILIO_AUTH_TOKEN).toString('base64');
-    // const voiceInput = await getTranscription(event.RecordingUrl, base64encodedData)
-
-    let voiceInput = event.SpeechResult;
-
-    const aiResponse = await generateAIResponses(voiceInput);
-
-    twiml.say({
-        voice: 'Polly.Mia-Neural'
-    }, aiResponse);
-
+    const thread_id = event.thread_id
+    let aiResponse = await generateAIResponses(event.SpeechResult, openai);
     const params = new URLSearchParams({ thread_id: thread_id });
 
-    twiml.redirect({
-        method: 'POST'
-    }, `/transcribe?${params}`);
+    if(aiResponse.includes('code_100')){
+        twiml.say({
+            voice: 'Polly.Mia-Neural'
+        }, aiResponse.replace('code_100', ''));
+        const transferTo = '+584125295840';
+        twiml.dial(transferTo)
+    } else {
+        twiml.say({
+            voice: 'Polly.Mia-Neural'
+        }, aiResponse);
+        twiml.redirect({
+            method: 'POST'
+        }, `/transcribe?${params}`)
+    }
 
     return callback(null, twiml);
 
-    // async function getTranscription(RecordingUrl, base64encodedData, filename = 'recording.mp3') {
-
-    //     try {
-
-    //         const response = await axios.get(RecordingUrl + '.mp3', {
-    //             responseType: 'stream',
-    //             headers: {
-    //                 'Authorization': `Basic ${base64encodedData}`
-    //             }
-    //         });
-          
-    //         if (response.status !== 200) {
-    //             throw new Error(`Failed to download file: ${response.statusText}`);
-    //         }
-
-    //         const audioBuffer = [];
-
-    //         response.data.on('data', (chunk) => audioBuffer.push(chunk));
-
-    //         await new Promise((resolve, reject) => {
-    //           response.data.on('end', resolve);
-    //           response.data.on('error', reject);
-    //         });
-
-    //         const audioData = Buffer.concat(audioBuffer);
-    //         const convertedFile = await toFile(Readable.from(audioData), filename);
-    //         const transcription = await openai.audio.transcriptions.create({
-    //             file: convertedFile,
-    //             model: "whisper-1",
-    //             language: "es",
-    //         });
-    //         return transcription.text || 'Disculpame, podrias repetir tu solicitud?'
-            
-    //     } catch (error) {
-    //         return 'Disculpame, podrias repetir tu solicitud?'
-    //     }
-    // }
-
-    async function generateAIResponses(voiceInput) {
+    async function generateAIResponses(voiceInput, openai) {
         openai.beta.threads.messages.create(thread_id, {
             role: "user",
             content: voiceInput
--- functions/transcribe.js
+++ functions/transcribe.js
@@ -3,12 +3,26 @@ const { OpenAI } = require("openai");
 exports.handler = async function (context, event, callback) {
     const openai = new OpenAI({ api_key: context.OPENAI_API_KEY});
     const twiml = new Twilio.twiml.VoiceResponse();
-    const firstInteract = 'Hola, mi nombre es Mia, como puedo ayudarte?'
+    const firstInteract = 'Hola, gracias por comunicarte con M&M Desarrollos, mi nombre es Mia, puedo proveer informacion acerca de apartamentos, casas o inversiones, como puedo ayudarte hoy?'
 
     let thread_id = event.thread_id || ''; 
+
+    const callInfo = {
+        caller_country: event.CallerCountry,
+        caller_state: event.CallerState,
+        caller_city: event.CallerCity,
+        call_id: event.CallSid,
+        caller_number: event.Caller,
+        date: new Date().toDateString()
+    }
+    console.log(callInfo);
     if(!thread_id) {
         const thread = await openai.beta.threads.create({
             messages: [
+                {
+                    "role": "assistant",
+                    "content": JSON.stringify(callInfo)+". You can use this JSON to obtain call information like the date of the call, the ID which will be the call_id, the phone number of the caller which will be the caller_number, and other data like the city and state"
+                },
                 {
                     "role": "assistant",
                     "content": firstInteract
@@ -21,13 +35,6 @@ exports.handler = async function (context, event, callback) {
         }, firstInteract);
     }
     const params = new URLSearchParams({ thread_id: thread_id });
-  
-    // twiml.record({
-    //     action: `/respond?${params}`,
-    //     timeout: 1,
-    //     maxLength: 60,
-    //     playBeep: false   
-    // });
 
     twiml.gather({
         enhanced: "true",
